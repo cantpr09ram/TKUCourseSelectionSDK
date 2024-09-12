85f96191063ed56bc8d20ab0a33ee71eb2f6e655
bochain <53370227+cantpr09ram@users.noreply.github.com>
2024-09-12 16:13:52 +0800

    Refactor captcha code and add captcha image download function

--- course_selection.py
+++ course_selection.py
@@ -3,21 +3,51 @@ import re
 
 import requests
 from parsel import Selector
+# tool use to get hash key
+from PIL import Image
+from io import BytesIO
+from bs4 import BeautifulSoup
 
+def download_and_show_captcha(html_content, img_id):
+    # 使用 BeautifulSoup 查找 <img> 標籤的 src 屬性
+    soup = BeautifulSoup(html_content, 'lxml')
+    img_tag = soup.find('img', id=img_id)
+    
+    if img_tag:
+        img_src = img_tag['src']
+        
+        # 判斷圖片URL是否是相對路徑或缺少部分路徑，若是則補全為完整URL
+        if not img_src.startswith("http"):
+            # 修正 URL，將 BaseData 放置在 EleCos 後面
+            img_src = img_src.replace("BaseData", "/EleCos/BaseData")
+            img_src = f"https://www.ais.tku.edu.tw{img_src}"
+        
+        print(f"Captcha URL: {img_src}")
+        
+        # 發送請求下載驗證碼圖片
+        response = requests.get(img_src)
+        if response.status_code == 200:
+            # 打開並顯示圖片
+            img = Image.open(BytesIO(response.content))
+            img.show()
+        else:
+            print(f"Failed to download captcha image.{response.status_code}")
+    else:
+        print(f"No image found with id: {img_id}")
 
 class TKUCourseSelector:
-    captcha_pattern = re.compile('^\[("[0-9a-z]{40}",?){6}\]$')
+    captcha_pattern = re.compile('^\[("[0-9a-z]{64}",?){6}\]$')
     captcha_mapping = {
-        'b6589fc6ab0dc82cf12099d1c2d40ab994e8410c': '0',
-        '356a192b7913b04c54574d18c28d46e6395428ab': '1',
-        'da4b9237bacccdf19c0760cab7aec4a8359010b0': '2',
-        '77de68daecd823babbb58edb1c8e14d7106e83bb': '3',
-        '1b6453892473a467d07372d45eb05abc2031647a': '4',
-        'ac3478d69a3c81fa62e60f5c3696165a4e5e6ac4': '5',
-        'c1dfd96eea8cc2b62785275bca38ac261256e278': '6',
-        '902ba3cda1883801594b6e1b452790cc53948fda': '7',
-        'fe5dbbcea5ce7e2988b8c69bcfdfde8904aabc1f': '8',
-        '0ade7c2cf97f75d009975f4d720d1fa6c19f4897': '9'
+        '4ae81572f06e1b88fd5ced7a1a000945432e83e1551e6f721ee9c00b8cc33260': '0',
+        'a9f51566bd6705f7ea6ad54bb9deb449f795582d6529a0e22207b8981233ec58': '1',
+        'fcb5f40df9be6bae66c1d77a6c15968866a9e6cbd7314ca432b019d17392f6f4': '2',
+        'e632b7095b0bf32c260fa4c539e9fd7b852d0de454e9be26f24d0d6f91d069d3': '3',
+        '559aead08264d5795d3909718cdd05abd49572e84fe55590eef31a88a08fdffd': '4',
+        'a83dd0ccbffe39d071cc317ddf6e97f5c6b1c87af91919271f9fa140b0508c6c': '5',
+        '8de0b3c47f112c59745f717a626932264c422a7563954872e237b223af4ad643': '6',
+        'a25513c7e0f6eaa80a3337ee18081b9e2ed09e00af8531c8f7bb2542764027e7': '7',
+        '6b23c0d5f35d1b11f9b683f0b0a617355deb11277d91ae091d399c655b87940d': '8',
+        '3f39d5c348e5b79d06e842c114e6cc571583bbf44e4b0ebfda1a01ec05745d43': '9'
     }
 
     def __init__(self):
@@ -30,6 +60,7 @@ class TKUCourseSelector:
 
     @staticmethod
     def get_captcha_code(text: str):
+        print(text)
         assert __class__.captcha_pattern.match(text) is not None, "captcha not match!"
         return ''.join(map(__class__.captcha_mapping.get, eval(text)))
 
